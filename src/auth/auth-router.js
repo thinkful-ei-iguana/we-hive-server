ade82e3307bc04d76be95f9fbba0de21cd5e7f77
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2021-06-14 15:32:17 -0700

    update knex and pg

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const AuthService = require("./auth-service");
-const requireAuth = require("../middleware/jwt-auth");
+const express = require('express');
+const AuthService = require('./auth-service');
+const requireAuth = require('../middleware/jwt-auth');
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
 authRouter
-  .route("/login")
+  .route('/login')
   .post(jsonBodyParser, (req, res, next) => {
     const { user_email, password } = req.body;
     const loginUser = { user_email, password };
@@ -13,31 +13,31 @@ authRouter
     for (const [key, value] of Object.entries(loginUser))
       if (!value) {
         return res.status(400).json({
-          error: `Missing '${key}' in request body`
+          error: `Missing '${key}' in request body`,
         });
       }
-    AuthService.getUserWithUserEmail(req.app.get("db"), loginUser.user_email)
+    AuthService.getUserWithUserEmail(req.app.get('db'), loginUser.user_email)
 
-      .then(dbUser => {
+      .then((dbUser) => {
         if (!dbUser) {
           return res.status(400).json({
-            error: "Incorrect user email or password"
+            error: 'Incorrect user email or password',
           });
         }
         return AuthService.comparePasswords(
           loginUser.password,
           dbUser.password
-        ).then(compareMatch => {
+        ).then((compareMatch) => {
           if (!compareMatch) {
             return res.status(400).json({
-              error: "Incorrect user email or password"
+              error: 'Incorrect user email or password',
             });
           }
           const sub = dbUser.user_email;
           const payload = { user_id: dbUser.id };
 
           res.send({
-            authToken: AuthService.createJwt(sub, payload)
+            authToken: AuthService.createJwt(sub, payload),
           });
         });
       })
@@ -47,10 +47,10 @@ authRouter
     const sub = req.user.user_email;
     const payload = {
       user_id: req.user.id,
-      name: req.user.name
+      name: req.user.name,
     };
     res.send({
-      authToken: AuthService.createJwt(sub, payload)
+      authToken: AuthService.createJwt(sub, payload),
     });
   });
 
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,33 +1,33 @@
 /* eslint-disable quotes */
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 function makeUsersArray() {
   return [
     {
       id: 1,
-      user_name: "test-user-1",
-      first_name: "Test User 1",
-      user_email: "tu1@gmail.com",
-      password: "Password1!",
-      date_created: "2020-01-01T12:30:30.615Z"
+      user_name: 'test-user-1',
+      first_name: 'Test User 1',
+      user_email: 'tu1@gmail.com',
+      password: 'Password1!',
+      date_created: '2020-01-01T12:30:30.615Z',
     },
     {
       id: 2,
-      user_name: "test-user-2",
-      first_name: "Test User 2",
-      user_email: "tu2@gmail.com",
-      password: "Password2!",
-      date_created: "2020-01-01T12:30:30.615Z"
+      user_name: 'test-user-2',
+      first_name: 'Test User 2',
+      user_email: 'tu2@gmail.com',
+      password: 'Password2!',
+      date_created: '2020-01-01T12:30:30.615Z',
     },
     {
       id: 3,
-      user_name: "test-user-3",
-      first_name: "Test User 3",
-      user_email: "tu3@gmail.com",
-      password: "Password3!",
-      date_created: "2020-01-01T12:30:30.615Z"
-    }
+      user_name: 'test-user-3',
+      first_name: 'Test User 3',
+      user_email: 'tu3@gmail.com',
+      password: 'Password3!',
+      date_created: '2020-01-01T12:30:30.615Z',
+    },
   ];
 }
 
@@ -36,27 +36,27 @@ function makeHivesArray() {
     {
       id: 1,
       goal_type: 3,
-      goal_description: "Goal 1 desc for type 3",
-      target_date: "2020-01-01T12:30:30.615Z",
-      group_message: "Goal 1 group message",
-      date_added: "2020-01-01T12:30:30.615Z"
+      goal_description: 'Goal 1 desc for type 3',
+      target_date: '2020-01-01T12:30:30.615Z',
+      group_message: 'Goal 1 group message',
+      date_added: '2020-01-01T12:30:30.615Z',
     },
     {
       id: 2,
       goal_type: 2,
-      goal_description: "Goal 2 desc for type 2",
-      target_date: "2020-01-01T12:30:30.615Z",
-      group_message: "Goal 2 group message",
-      date_added: "2020-01-01T12:30:30.615Z"
+      goal_description: 'Goal 2 desc for type 2',
+      target_date: '2020-01-01T12:30:30.615Z',
+      group_message: 'Goal 2 group message',
+      date_added: '2020-01-01T12:30:30.615Z',
     },
     {
       id: 3,
       goal_type: 1,
-      goal_description: "Goal 3 desc for type 1",
-      target_date: "2020-01-01T12:30:30.615Z",
-      group_message: "",
-      date_added: "2020-01-01T12:30:30.615Z"
-    }
+      goal_description: 'Goal 3 desc for type 1',
+      target_date: '2020-01-01T12:30:30.615Z',
+      group_message: '',
+      date_added: '2020-01-01T12:30:30.615Z',
+    },
   ];
 }
 
@@ -65,29 +65,29 @@ function makeHivesUsersArray() {
     {
       id: 1,
       hive_id: 1,
-      user_id: 1
+      user_id: 1,
     },
     {
       id: 2,
       hive_id: 1,
-      user_id: 2
+      user_id: 2,
     },
     {
       id: 3,
       hive_id: 3,
-      user_id: 1
+      user_id: 1,
     },
     {
       id: 4,
       hive_id: 2,
-      user_id: 2
+      user_id: 2,
     },
     {
       id: 5,
       hive_id: 2,
       user_id: 2,
-      code: "testpass"
-    }
+      code: 'testpass',
+    },
   ];
 }
 
@@ -95,36 +95,36 @@ function makeActivityArray() {
   return [
     {
       id: 1,
-      action: "action for hive 1",
-      notes: "comment for hive 1",
-      date_added: "2020-01-01T12:30:30.615Z",
+      action: 'action for hive 1',
+      notes: 'comment for hive 1',
+      date_added: '2020-01-01T12:30:30.615Z',
       hive_id: 1,
-      user_id: 1
+      user_id: 1,
     },
     {
       id: 2,
-      action: "another action for hive 1",
-      notes: "another comment for hive 1",
-      date_added: "2020-01-01T12:30:30.615Z",
+      action: 'another action for hive 1',
+      notes: 'another comment for hive 1',
+      date_added: '2020-01-01T12:30:30.615Z',
       hive_id: 1,
-      user_id: 2
+      user_id: 2,
     },
     {
       id: 3,
-      action: "third action for hive 1",
-      notes: "third comment for hive 1",
-      date_added: "2020-01-01T12:30:30.615Z",
+      action: 'third action for hive 1',
+      notes: 'third comment for hive 1',
+      date_added: '2020-01-01T12:30:30.615Z',
       hive_id: 1,
-      user_id: 3
+      user_id: 3,
     },
     {
       id: 4,
-      action: "first action for hive 2",
-      notes: "first comment for hive 2",
-      date_added: "2020-01-01T12:30:30.615Z",
+      action: 'first action for hive 2',
+      notes: 'first comment for hive 2',
+      date_added: '2020-01-01T12:30:30.615Z',
       hive_id: 2,
-      user_id: 1
-    }
+      user_id: 1,
+    },
   ];
 }
 
@@ -135,7 +135,7 @@ function makeExpectedHive(hive) {
     goal_description: hive.goal_description,
     target_date: hive.target_date,
     group_message: hive.group_message,
-    date_added: hive.date_added
+    date_added: hive.date_added,
   };
 }
 
@@ -143,14 +143,14 @@ function makeExpectedJoinHive(user, hive) {
   return {
     id: 1,
     hive_id: hive.id,
-    user_id: user.id
+    user_id: user.id,
   };
 }
 
 function makeExpectedActivity(users, hivesUser, activity = []) {
-  const user = users.find(user => user.id === hivesUser.user_id);
+  const user = users.find((user) => user.id === hivesUser.user_id);
   const activityList = activity.filter(
-    act => act.hive_id === hivesUser.hive_id
+    (act) => act.hive_id === hivesUser.hive_id
   );
 
   return {
@@ -160,7 +160,7 @@ function makeExpectedActivity(users, hivesUser, activity = []) {
     date_added: activity.date_added,
     hive_id: activity.hive_id,
     user_id: activity.user_id,
-    user: user.first_name
+    user: user.first_name,
   };
 }
 
@@ -173,7 +173,7 @@ function makeHivesFixtures() {
 }
 
 function cleanTables(db) {
-  return db.transaction(trx =>
+  return db.transaction((trx) =>
     trx.raw(
       `TRUNCATE hive_activity, hives_users, hives, users RESTART IDENTITY CASCADE`
     )
@@ -181,13 +181,13 @@ function cleanTables(db) {
 }
 
 function seedUsers(db, users) {
-  const preppedUsers = users.map(user => ({
+  const preppedUsers = users.map((user) => ({
     ...user,
-    password: bcrypt.hashSync(user.password, 1)
+    password: bcrypt.hashSync(user.password, 1),
   }));
 
   return db
-    .into("users")
+    .into('users')
     .insert(preppedUsers)
     .then(() =>
       db.raw(`SELECT setval('users_id_seq', ?)`, [users[users.length - 1].id])
@@ -196,7 +196,7 @@ function seedUsers(db, users) {
 
 function seedHives(db, hives) {
   return db
-    .into("hives")
+    .into('hives')
     .insert(hives)
     .then(() =>
       db.raw(`SELECT setval('hives_id_seq', ?)`, [hives[hives.length - 1].id])
@@ -205,29 +205,29 @@ function seedHives(db, hives) {
 
 function seedHivesUsers(db, hives_users) {
   return db
-    .into("hives_users")
+    .into('hives_users')
     .insert(hives_users)
     .then(() =>
       db.raw(`SELECT setval('hives_users_id_seq', ?)`, [
-        hives_users[hives_users.length - 1].id
+        hives_users[hives_users.length - 1].id,
       ])
     );
 }
 function seedHivesTables(db, users, hives, hives_users, activity = []) {
   //use a transaction to group the queries and auto rollback on any failure
-  return db.transaction(async trx => {
+  return db.transaction(async (trx) => {
     seedUsers(trx, users);
     seedHives(trx, hives);
     seedHivesUsers(trx, hives_users);
 
     trx.raw(`SELECT setval('hives_users_id_seq', ?)`, [
-      hives_users[hives_users.length - 1].id
+      hives_users[hives_users.length - 1].id,
     ]);
 
     if (activity.length) {
-      await trx.into("hive_activity").insert(activity);
+      await trx.into('hive_activity').insert(activity);
       await trx.raw(`SELECT setval('hive_activity_id_seq', ?)`, [
-        activity[activity.length - 1].id
+        activity[activity.length - 1].id,
       ]);
     }
   });
@@ -236,7 +236,7 @@ function seedHivesTables(db, users, hives, hives_users, activity = []) {
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
   const token = jwt.sign({ user_id: user.id }, secret, {
     subject: user.user_email,
-    algorithm: "HS256"
+    algorithm: 'HS256',
   });
   return `Bearer ${token}`;
 }
@@ -260,5 +260,5 @@ module.exports = {
   seedHivesTables,
   seedHivesUsers,
   makeAuthHeader,
-  makeHashPassword
+  makeHashPassword,
 };
