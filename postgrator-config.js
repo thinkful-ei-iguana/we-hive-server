14247427b47a905476c77665e6632eeaacb10be0
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2021-06-16 15:31:55 -0700

    add ssl connection

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-  migrationDirectory: "migrations",
-  driver: "pg",
+  migrationDirectory: 'migrations',
+  driver: 'pg',
   connectionString:
-    process.env.NODE_ENV === "TEST"
+    process.env.NODE_ENV === 'TEST'
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
-  ssl: !!process.env.SSL
+  ssl: process.env.DATABASE_URL ? true : false,
 };
