0113dfaf1e287292327ecda29315f294a7e729e1
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2021-06-16 14:13:27 -0700

    upgrade to  heroku-20 stack

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  PORT: process.env.PORT || 8080,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://hive_admin@localhost/we_hive",
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    "postgresql://hive_admin@localhost/we_hive_test",
-  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
+    'postgresql://hive_admin@localhost/we_hive_test',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
--- src/server.js
+++ src/server.js
@@ -10,5 +10,7 @@ const db = knex({
 app.set('db', db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost: ${PORT}, ${DATABASE_URL}`);
+  console.log(
+    `Server listening at http://localhost: ${PORT},(${process.env.PORT}) ${process.env.DATABASE_URL}, ${db}`
+  );
 });
